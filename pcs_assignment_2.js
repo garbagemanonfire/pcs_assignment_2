a88e01e4f7a99a67207ce183bd832e831b4965e8
garbagemanonfire <nilesvm@gmail.com>
2013-10-10 16:47:34 -0700

    This is the final

--- pcs_assignment_2.js
+++ pcs_assignment_2.js
@@ -1,14 +1,51 @@
 var myString 
+var myOutput
 
-myString = 'Hello';
-
-function removeVowels(string) {
-   return string.replace(/[aeiou]/gi,'')
-}
-
-removeVowels(myString);
-console.log(myString);
+myString = 'HEllAouiOIT';
+myOutput = ""
 
 for (var i = myString.length-1; i >= 0; i --) {
-  console.log(myString[i]);
+  if ((myString[i] === 'a')||(myString[i] === 'A')){
+  	myString[i] = ""
+  }
+  else if ((myString[i] === 'e')||(myString[i] === 'E')) {
+  	myString[i] = ""
+  }
+   else if ((myString[i] === 'i')||(myString[i] === 'I')){
+  	myString[i] = ""
+  }
+   else if ((myString[i] === 'o')||(myString[i] === 'O')){
+  	myString[i] = ""
+  }
+  else if ((myString[i] === 'u')||(myString[i] === 'U')) {
+  	myString[i] = ""
+  }
+  else { 
+  	console.log(myString[i])
+  	myOutput = myOutput + myString[i]
+	}
 }
+
+console.log(myOutput)
+
+
+
+// w switch
+// for (var i = myString.length-1; i >= 0; i --) {
+//   switch(myString[i]) {}
+//   case "a": 
+//   	console.log("found an a");
+//   case "l":
+//   	console.log(myString[i]);
+//   }
+// }
+
+
+// function removeVowels(string) {
+// 	var retVal = string.replace(/[aeiou]/gi,'');
+// 	console.log(retVal);
+// 	return retVal;
+// }
+
+// myString = removeVowels(myString);
+// console.log(myString);
